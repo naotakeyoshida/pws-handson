7efc6cfaf9e32633b05cee05bfb0b837efb83e5e
naotakeyoshida <naotake.yoshida@emc.com>
2017-03-02 21:04:30 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,12 @@ echo "============================="
 echo `git --version`
 echo "============================="
 
+## Clone Repo
+cd /home/vagrant
+git clone https://github.com/naotakeyoshida/pws-handson1.git
+git clone https://github.com/naotakeyoshida/pws-handson2.git
+git clone https://github.com/naotakeyoshida/pws-handson3.git
+
 ## Install CF CLI
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 sudo mv cf /usr/local/bin
